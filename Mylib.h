7d48e766a776cb370d081080978d2e98e97d7182
darkskill33 <116464783+darkskill33@users.noreply.github.com>
2023-05-30 23:24:49 +0300

    Add files via upload

---
+++ Mylib.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+#include <limits>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <sstream> 
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <list>
+#include <deque>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <map>
+#include <regex>
+#include <locale>
+#include <unordered_map>
+
+
+
+
+using std::istream;
+using std::ostream;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::fixed;
+using std::string;
+using std::copy;
+using std::swap;
+using std::vector;
+using std::min;
+using std::stoi;
+using std::remove_if;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::sort;
+using std::list;
+using std::deque;
+using std::map;
+using std::stringstream;
+using std::regex;
+using std::locale;
+using std::pair;
+using std::unordered_map;
+
